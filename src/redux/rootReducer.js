bbd1968fb9ee87259911d5bf4410c07dcfe309a1
Mauro Insacco <mauro.insacco@gmail.com>
2019-10-26 17:25:13 +0200

    added privateroute and redux auth reducer/actions

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import chartReducer from './reducers/chart.reducer';
+import authReducer from './reducers/auth.reducer';
 
 export const rootReducer = combineReducers({
-    chart: chartReducer
+    chart: chartReducer,
+    auth: authReducer
 });
