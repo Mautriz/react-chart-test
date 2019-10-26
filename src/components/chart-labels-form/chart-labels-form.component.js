c0b4789ab7b8c6cf60f511df93102aa5fd918072
Mauro Insacco <mauro.insacco@gmail.com>
2019-10-26 14:48:23 +0200

    labels form component update, dispatching correctly, needs validation

--- src/components/chart-labels-form/chart-labels-form.component.js
+++ src/components/chart-labels-form/chart-labels-form.component.js
@@ -1,9 +1,61 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { chartUpdateLabels } from '../../redux/actions/chart.actions';
 
+// Normalmente per i form userei una libreria (Formik), uso vanilla per far vedere che so come funziona
 class ChartLabelsForm extends Component {
+    state = {
+        xAxesLabel: '',
+        yAxesLabel: '',
+        xAxesLabelTouched: false,
+        yAxesLabelTouched: false,
+        isFormValid: true
+    };
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    handleBlur = e => {
+        const statePiece = `${e.target.name}Touched`;
+        this.setState({ [statePiece]: true });
+    };
+
+    handleSubmit = e => {
+        const { xAxesLabel, yAxesLabel } = this.state;
+        e.preventDefault();
+        if (this.state.isFormValid) {
+            this.props.chartUpdateLabels({
+                xAxesLabel,
+                yAxesLabel
+            });
+        }
+    };
+
     render() {
-        return <div>Chart labels form wokrs</div>;
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor="xAxesLabel">xAxes</label>
+                <input
+                    type="text"
+                    name="xAxesLabel"
+                    onChange={this.handleChange}
+                    onBlur={this.handleBlur}
+                ></input>
+                <label htmlFor="xAxesLabel">xAxes</label>
+                <input
+                    type="text"
+                    name="yAxesLabel"
+                    onChange={this.handleChange}
+                    onBlur={this.handleBlur}
+                ></input>
+                <button type="submit">SET LABELS</button>
+            </form>
+        );
     }
 }
 
-export default ChartLabelsForm;
+export default connect(
+    null,
+    { chartUpdateLabels }
+)(ChartLabelsForm);
