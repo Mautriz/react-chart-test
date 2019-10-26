577571f5d6dfefdcf40ccddb9a7c4bb2450a0663
Mauro Insacco <mauro.insacco@gmail.com>
2019-10-26 17:40:21 +0200

    added auth persistance and logout button

--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ChartLabelsForm from '../../components/chart-labels-form/chart-labels-form.component';
 import ChartLoadJson from '../../components/chart-load-json/chart-load-json.component';
 import LineChart from '../../components/line-chart/line-chart.component';
+import { authLogout } from '../../redux/actions/auth.actions';
+import { connect } from 'react-redux';
 
-const HomePage = () => {
+const HomePage = ({ authLogout }) => {
     return (
         <div>
             <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
@@ -11,8 +13,19 @@ const HomePage = () => {
                 <ChartLabelsForm />
             </div>
             <LineChart />
+            <button
+                style={{ position: 'fixed', right: '15px', bottom: '15px' }}
+                onClick={authLogout}
+            >
+                PRESS HERE TO LOGOUT
+            </button>
         </div>
     );
 };
 
-export default HomePage;
+export default connect(
+    null,
+    {
+        authLogout
+    }
+)(HomePage);
--- src/redux/reducers/auth.reducer.js
+++ src/redux/reducers/auth.reducer.js
@@ -1,14 +1,16 @@
 import { authTypes } from '../types/auth.types';
 
 const initialState = {
-    isAuthenticated: false
+    isAuthenticated: !!window.localStorage.getItem('isAuthenticated')
 };
 
 export default (state = initialState, { type, payload }) => {
     switch (type) {
         case authTypes.LOGIN:
+            window.localStorage.setItem('isAuthenticated', 'randomvalue');
             return { ...state, isAuthenticated: true };
         case authTypes.LOGOUT:
+            window.localStorage.removeItem('isAuthenticated');
             return { ...state, isAuthenticated: false };
         default:
             return state;
