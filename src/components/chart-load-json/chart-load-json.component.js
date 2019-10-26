4405ecba955e26bc7b861abf963fbefbf2341899
Mauro Insacco <mauro.insacco@gmail.com>
2019-10-26 15:03:08 +0200

    json load form implemented, added chart.js, removed styled-components

--- src/components/chart-load-json/chart-load-json.component.js
+++ src/components/chart-load-json/chart-load-json.component.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { chartUpdateValues } from '../../redux/actions/chart.actions';
 
 class ChartLoadJson extends Component {
+    state = {
+        chartValues: {}
+    };
+
+    handleChange = e => {
+        const chartValues = this.parseJsonValidation(e.target.value);
+        this.setState({ chartValues });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.chartUpdateValues(this.state.chartValues);
+    };
+
+    parseJsonValidation = json => {
+        try {
+            return JSON.parse(json);
+        } catch (e) {
+            return {};
+        }
+    };
+
     render() {
-        return <div>Chart load json works</div>;
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <textarea
+                    onChange={this.handleChange}
+                    name="jsonData"
+                ></textarea>
+                <button type="submit">LOAD JSON</button>
+            </form>
+        );
     }
 }
 
-export default ChartLoadJson;
+export default connect(
+    null,
+    { chartUpdateValues }
+)(ChartLoadJson);
